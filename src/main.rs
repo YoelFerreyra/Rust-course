928fd50b7f18affcda2dde193a52ee95db0621ad
yoelferreyra <yoelferreyra24@gmail.com>
2024-04-15 08:12:22 -0300

    Adding new files to the repository

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("Hello word I'm Yoel");
+    car_function()
+}
+
+fn car_function (){
+    let car = "BMW";
+    let speed = 150;
+
+    print!("{car}");
+    print!("{speed}");
+
+    print!("The {car} has the speed of {speed}")
+}
